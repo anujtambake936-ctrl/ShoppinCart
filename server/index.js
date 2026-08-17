a7f8f78a10145ee0fed6e2b15a483919936253f8
Anuj <anujtambake936@gmail.com>
2026-08-17 19:59:17 +0530

    Mongodb issue fixed

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]); // Overrides local system DNS for Node.js
+
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
